4af2f35d1d7025007869f845010d6a02e8dd644d
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-10 10:41:20 +0530

    new one added

---
+++ NewDirectory1/Array_part2.cs
@@ -0,0 +1,33 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Array_part
+//     {
+//         public static void Main(string[] args)
+//         {
+//             int input1 = int.Parse(Console.ReadLine());
+//             int[] arr=new int[input1];
+//             for (int i = 0; i < arr.Length; i++) {
+//                 arr[i] = int.Parse(Console.ReadLine());
+//             }
+//
+//             int Totalsum=0;
+//             for (int i = 0; i < arr.Length; i++) {
+//                 Totalsum+=arr[i];
+//             }
+//
+//             int count = 0;
+//             int preSum = 0;
+//             for (int i = 0; i < arr.Length-1; i++) {
+//                 preSum=preSum+arr[i];
+//                 
+//                 int secondSum=Totalsum-preSum;
+//                 if (Math.Abs(secondSum-preSum)%2==0) {
+//                     count++;
+//                 }
+//             }
+//             Console.WriteLine(count);
+//         }
+//     }
+// }
