96b71024951ced4404d22cfcda2cb214cbd60684
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-13 20:17:33 +0530

    new one add

---
+++ NewDirectory1/StartingEndingPair8.cs
@@ -0,0 +1,37 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class StartingEndingPair8
+//     {
+//         public static void Main(string[] args)
+//         {
+//             string str = Console.ReadLine();
+//             Dictionary<string, int> map = new Dictionary<string, int>();
+//
+//             foreach (string s in str.Split(' '))
+//             {
+//                 string hp = s[0] + "" + "" + s[s.Length - 1];
+//                 if (map.ContainsKey(hp))
+//                 {
+//                     map[hp]++;
+//                 }
+//                 else
+//                 {
+//                     map.Add(hp, 1);
+//                 }
+//             }
+//
+//             int max = map.Values.Max();
+//             foreach (var m in map.Keys)
+//             {
+//                 if (map[m]== max)
+//                 {
+//                     Console.WriteLine(m);
+//                 }
+//             }
+//         }
+//     }
+// }
