e34a22c014410334330dbe24d5e093554b58ea93
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-14 21:08:54 +0530

    new one add

---
+++ NewDirectory1/LogicalOr7.cs
@@ -0,0 +1,16 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class LogicalOr
+//     {
+//         public static void Main(string[] args)
+//         {
+//             int num1=int.Parse(Console.ReadLine());
+//             int num2=int.Parse(Console.ReadLine());
+//             
+//             int result = num1 | num2;
+//             Console.WriteLine(result);
+//         }
+//     }
+// }
