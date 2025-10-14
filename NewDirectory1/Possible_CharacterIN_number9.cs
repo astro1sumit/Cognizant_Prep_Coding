7a1839e9b4f9f9367d26a6945893e8a523f1b5dd
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-14 21:21:26 +0530

    new one add

---
+++ NewDirectory1/Possible_CharacterIN_number9.cs
@@ -0,0 +1,25 @@
+﻿// using System;
+// using System.Diagnostics.CodeAnalysis;
+//
+// namespace ConsoleApplication1
+// {
+//     public class Possible_CharacterIN_number
+//     {
+//         public static void Main(string[] args)
+//         {
+//             string number = Console.ReadLine();
+//             int count = number.Length;
+//
+//             
+//             for (int i = 0; i < number.Length-1; i++)
+//             {
+//                 int sum = int.Parse(number[i].ToString())*10 + int.Parse(number[i+1].ToString());
+//                 if (sum <= 26)
+//                 {
+//                     count++;
+//                 }
+//             }
+//             Console.WriteLine(count);
+//         }
+//     }
+// }
