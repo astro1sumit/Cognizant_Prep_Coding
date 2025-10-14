bfb78e45d6d924002f81eebef4a74b6817e40a7e
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-14 11:45:56 +0530

    new one add

---
+++ NewDirectory1/nth_prime.cs
@@ -0,0 +1,41 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class nth_prime
+//     {
+//         public static void Main(string[] args)
+//         {
+//             int count = 0;
+//             int num = 1;
+//             while (count != 6)
+//             {
+//                 num++;
+//                 if (isPrime(num))
+//                 {
+//                     count++;
+//                 }
+//             }
+//
+//             Console.WriteLine(num);
+//         }
+//
+//         public static Boolean isPrime(int num)
+//         {
+//             if (num == 2)
+//             {
+//                 return true;
+//             }
+//
+//             for (int i = 2; i <= num / 2; i++)
+//             {
+//                 if (num % i == 0)
+//                 {
+//                     return false;
+//                 }
+//             }
+//             return true;
+//         }
+//         
+//     }
+// }
