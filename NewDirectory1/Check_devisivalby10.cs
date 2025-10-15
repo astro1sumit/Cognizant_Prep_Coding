f7c4ba1e1d4bf907a87615526c5c00a59d4611bc
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-15 11:25:19 +0530

    new one add

---
+++ NewDirectory1/Check_devisivalby10.cs
@@ -0,0 +1,28 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_devisivalby10
+//     {
+//         public static void Main(string[] args)
+//         {
+//             int size=int.Parse(Console.ReadLine());
+//             int[] arr= new int[size];
+//
+//             for (int i = 0; i < size; i++)
+//             {
+//                 arr[i] = int.Parse(Console.ReadLine());
+//             }
+//
+//             int count = 0;
+//             for (int i = 0; i < size; i++)
+//             {
+//                 if (arr[i] % 10 == 0)
+//                 {
+//                     count++;
+//                 }
+//             }
+//             Console.WriteLine(count);
+//         }
+//     }
+// }
