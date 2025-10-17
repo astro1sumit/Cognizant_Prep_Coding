ebe9fd7f8212496af234bf1d7ab449d7a98779ea
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-17 22:32:55 +0530

    new one add

---
+++ even_odd_difference13.cs
@@ -0,0 +1,33 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+//
+// namespace ConsoleApplication1
+// {
+//     public class even_odd_difference13
+//     {
+//         public static void Main(string[] args)
+//         {
+//             string str = Console.ReadLine();
+//
+//             Dictionary<char, int> map = new Dictionary<char, int>();
+//
+//             for (int i = 0; i < str.Length; i++)
+//             {
+//                 if (map.ContainsKey(str[i]))
+//                 {
+//                     map[str[i]]++;
+//                 }
+//                 else
+//                 {
+//                     map.Add(str[i],1);
+//                 }
+//             }
+//
+//             int max = map.Values.Max();
+//             int min = map.Values.Min();
+//             int diff = max - min;
+//             Console.WriteLine(diff);
+//         }
+//     }
+// }
