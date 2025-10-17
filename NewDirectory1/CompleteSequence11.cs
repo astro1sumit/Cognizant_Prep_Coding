20e869095487fa8dbce8a6681eff67f75e717823
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-17 11:36:51 +0530

    new one add

---
+++ NewDirectory1/CompleteSequence11.cs
@@ -0,0 +1,33 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class CompleteSequence11
+//     {
+//         public static void Main(string[] args)
+//         {
+//             int terms = int.Parse(Console.ReadLine());
+//             int[] seq = new int[6];
+//
+//             for (int i = 0; i < 6; i++)
+//             {
+//                 seq[i] = int.Parse(Console.ReadLine());
+//             }
+//             int[] result = new int[terms];
+//             for (int i = 0; i < 6; i++)
+//             {
+//                 result[i] = seq[i];
+//             }
+//
+//             for (int i = 6; i < terms; i++)
+//             {
+//                 result[i]=result[i-1]+result[i-2];
+//             }
+//
+//             for (int i = 0; i < terms; i++)
+//             {
+//                 Console.Write(result[i]+" ");
+//             }
+//         }
+//     }
+// }
