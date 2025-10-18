0d552109c52b34bd3c6a02b0075578bc8121ddd5
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-18 07:27:31 +0530

    new one add

---
+++ NewDirectory1/Check_fibo.cs
@@ -0,0 +1,19 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_fibo {
+//         public static void Main(string[] args) {
+//             int num = int.Parse(Console.ReadLine());
+//             for (int i = 0; i <= num; i++) {
+//                 Console.WriteLine(fibo(i));
+//             }
+//         }
+//         public static int fibo(int num) {
+//             if (num == 0 || num == 1) {
+//                 return num;
+//             }
+//             return fibo(num - 1) + fibo(num - 2);
+//         }
+//     }
+// }
