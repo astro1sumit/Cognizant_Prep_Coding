1336fe0cec99fee49a686fbd777b84029530522d
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-20 23:33:29 +0530

    new one add

---
+++ NewDirectory1/Check_First_Non_Duplicate.cs
@@ -0,0 +1,35 @@
+﻿// using System;
+// using System.Collections.Generic;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_First_Non_Duplicate
+//     {
+//         public static void Main(string[] args)
+//         {
+//             string str = Console.ReadLine();
+//             Dictionary<char, int> map=new Dictionary<char, int>();
+//
+//             for (int i = 0; i < str.Length; i++)
+//             {
+//                 if (map.ContainsKey(str[i]))
+//                 {
+//                     map[str[i]]++;
+//                 }
+//                 else
+//                 {
+//                     map[str[i]] = 1;
+//                 }
+//             }
+//
+//             foreach (var items in map)
+//             {
+//                 if (items.Value == 1)
+//                 {
+//                     Console.WriteLine(items.Key);
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// }
