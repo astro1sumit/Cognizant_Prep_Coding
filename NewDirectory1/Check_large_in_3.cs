4a877bd1ec3c7210f9a4ccd9077fb84ede86d1dc
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-21 21:54:05 +0530

    new one add

---
+++ NewDirectory1/Check_large_in_3.cs
@@ -0,0 +1,29 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_large_in_3
+//     {
+//         public static void Main(string[] args)
+//         {
+//             int num1 = int.Parse(Console.ReadLine());
+//             int num2 = int.Parse(Console.ReadLine());
+//             int num3 = int.Parse(Console.ReadLine());
+//             int count = 0;
+//             if (num1 > num2)
+//             {
+//                 count = num1;
+//             }
+//             else
+//             {
+//                 count = num2;
+//             }
+//
+//             if (num3 > count)
+//             {
+//                 count = num3;
+//             }
+//             Console.WriteLine(count);
+//         }
+//     }
+// }
