a55079cf331cd7b5d9e44cabd9f2faa2ffcf6891
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-22 21:49:26 +0530

    new one add

---
+++ NewDirectory1/Check_Swap_Devide_Or_Multiply.cs
@@ -0,0 +1,26 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_Swap_Devide_Or_Multiply
+//     {
+//         public static void Main(string[] args)
+//         {
+//             int num1=int.Parse(Console.ReadLine());
+//             int num2=int.Parse(Console.ReadLine());
+//
+//             int temp1 = num1 * num2;
+//             int temp2 = num1 * num2;
+//
+//             Console.WriteLine("a:"+temp1 / num1);
+//             Console.WriteLine("b:"+temp2 / num2);
+//             
+//         }
+//     }
+// }
+
+// a=3
+// b=4
+// a=a+b
+// b=a-b
+// a=a-b
