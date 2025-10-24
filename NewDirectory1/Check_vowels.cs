33edc3dc952523478744f8881e142cd1f5ac6d9c
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-25 00:13:50 +0530

    new one add

---
+++ NewDirectory1/Check_vowels.cs
@@ -0,0 +1,23 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_vowels
+//     {
+//         public static void Main(string[] args)
+//         {
+//             string str = Console.ReadLine();
+//             str=str.ToLower();
+//             int count = 0;
+//             
+//             for (int i = 0; i < str.Length; i++)
+//             {
+//                 if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+//                 {
+//                     count++;
+//                 }
+//             }
+//             Console.WriteLine(count);
+//         }
+//     }
+// }
