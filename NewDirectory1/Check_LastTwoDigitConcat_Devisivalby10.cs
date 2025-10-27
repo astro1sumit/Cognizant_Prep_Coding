4a2a55e8d04867b3ace6d475b7e02e40ebc0cce9
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-27 23:47:13 +0530

    new one add

---
+++ NewDirectory1/Check_LastTwoDigitConcat_Devisivalby10.cs
@@ -0,0 +1,20 @@
+﻿//
+// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_LastTwoDigitConcat_Devisivalby10 {
+//         public static void Main(string[] args) {
+//             int[] arr = { 15, 20 };
+//             int n = 0;
+//             for (int i = 0; i < arr.Length; i++) {
+//                 n = n * 10 + arr[i] % 10;
+//             }
+//             if (n % 10 == 0) {
+//                 Console.WriteLine(n);
+//             }else {
+//                 Console.WriteLine("No");
+//             }
+//         }
+//     }
+// }
