4515876b5f09aa9ad77e9e36814d686d2781c071
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-28 12:54:03 +0530

    new one add

---
+++ NewDirectory1/Check_Prime.cs
@@ -0,0 +1,22 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_Prime {
+//         public static void Main(string[] args) {
+//             int number = int.Parse(Console.ReadLine());
+//             int prime = 0;
+//             for (int i = 2; i <= number / 2; i++) {
+//                 if (number % i == 0) {
+//                     prime++;
+//                     break;
+//                 }
+//             }
+//             if (prime == 0) {
+//                 Console.WriteLine("Prime");
+//             }else {
+//                 Console.WriteLine("Not Prime");
+//             }
+//         }
+//     }
+// }
