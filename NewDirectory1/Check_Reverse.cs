92adebb20eaedecc06b3c4a22969a802cf6be3de
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-28 12:57:12 +0530

    new one add

---
+++ NewDirectory1/Check_Reverse.cs
@@ -0,0 +1,37 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_Reverse
+//     {
+//         public static void Main(string[] args)
+//         {
+//             string str=Console.ReadLine();
+//             string result="";
+//             for (int i = str.Length-1;i>=0; i--)
+//             {
+//                 result+=str[i];
+//             }
+//             Console.WriteLine(result);
+//         }
+//     }
+// }
+
+// Using Stringbuilder
+
+// using System;
+// using System.Text;
+//
+// namespace ConsoleApplication1.NewDirectory1 {
+//     public class Check_Reverse {
+//         public static void Main(string[] args) {
+//             string str=Console.ReadLine();
+//             StringBuilder result = new StringBuilder();
+//             for (int i = str.Length - 1; i >= 0; i--)
+//             {
+//                 result.Append(str[i]);
+//             }
+//             Console.WriteLine(result);
+//         }
+//     }
+// }
