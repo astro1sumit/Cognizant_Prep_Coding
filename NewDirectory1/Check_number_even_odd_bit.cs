276b6dc92a8812397f4207b535300f971c566128
astro1sumit <astro.sumit.usa@gmail.com>
2025-10-28 12:47:44 +0530

    new one add

---
+++ NewDirectory1/Check_number_even_odd_bit.cs
@@ -0,0 +1,15 @@
+﻿// using System;
+//
+// namespace ConsoleApplication1.NewDirectory1
+// {
+//     public class Check_number_even_odd_bit {
+//         public static void Main(string[] args) {
+//             int num = int.Parse(Console.ReadLine());
+//             if ((num & 1) == 1) {
+//                 Console.WriteLine("odd");
+//             }else {
+//                 Console.WriteLine("even");
+//             }
+//         }
+//     }
+// }
